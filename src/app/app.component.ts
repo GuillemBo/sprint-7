aca340a7c7f3371e38504cd09c765e163eb8457e
Guillem <guillemb4b@gmail.com>
2024-07-17 13:19:57 +0200

    header and showing list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StarshipListComponent } from "./components/starship-list/starship-list.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { StarshipDetailsComponent } from "./components/starship-details/starship-details.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StarshipListComponent],
+  imports: [RouterOutlet, StarshipListComponent, HeaderComponent, StarshipDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
