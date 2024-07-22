458198557fab12a2f6294b7c2e4f46a589aa430d
Guillem <guillemb4b@gmail.com>
2024-07-22 13:06:23 +0200

    home routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { StarshipDetailsComponent } from './components/starship-details/starship-details.component';
 import { StarshipListComponent } from './components/starship-list/starship-list.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/starships', pathMatch: 'full' },
   { path: '', component: StarshipListComponent },
   { path: 'details', component: StarshipDetailsComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: '' }
 ];
